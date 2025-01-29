e15e623e9b4cff4538150ff26de9cf37fac8103a
Arcsin Studios <arcsinstudios@outlook.com>
2025-01-29 21:22:32 +0800

    Modified index.html, about-us/index.html, contact-us/index.html, styles.css and scripts.js

--- scripts.js
+++ scripts.js
@@ -15,6 +15,12 @@ if (window.CSS) {
 
 function toggleTheme() {
     var page = document.getElementById("page");
+    var theme = localStorage.getItem("theme");
+    if (theme === null || theme === "light") {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
     page.classList.toggle("light-background");
     page.classList.toggle("dark-background");
 }

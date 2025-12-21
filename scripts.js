757d94c63b4b433578c3c7c8d7114f1fb3991e70
Arcsin Studios <arcsinstudios@outlook.com>
2025-12-21 13:39:25 +0800

    Added generators; refactored code; fixed known issues

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+"use strict";
+
+/* vvv CSS vvv */
 if (window.CSS) {
     CSS.registerProperty({
         name: "--start-stop",
@@ -12,90 +15,55 @@ if (window.CSS) {
         initialValue: "transparent"
     });
 }
+/* ^^^ CSS ^^^ */
 
+/* vvv GENERATION vvv */
+genLangBtn();
+genThemeBtn();
+genTitle();
+genSubtitle();
+genPageBtn();
+genSearch();
+genFooter();
+/* ^^^ GENERATION ^^^ */
+
+/* vvv VARIABLE vvv */
+/* vvv SWITCH vvv */
 var searchBoxVisible = false;
+/* ^^^ SWITCH ^^^ */
 
+/* vvv ELEMENT vvv */
+var page = document.getElementById("page");
+var pageLeft = document.getElementById("page-left");
+var pageRight = document.getElementById("page-right");
+var footer = document.getElementById("footer");
+/* ^^^ ELEMENT ^^^ */
+
+/* vvv PAGE vvv */
 var currPage = 1;
 var minPage = 1;
 var maxPage = 3;
-
 var pages = [];
-for (let i = minPage; i <= maxPage; ++i) {
-    pages.push(document.getElementById("page" + i));
-}
+/* ^^^ PAGE ^^^ */
 
+/* vvv LOCAL STORAGE vvv */
+var theme = localStorage.getItem("theme");
 var stupid = localStorage.getItem("stupid");
-if (stupid === null) {
-    stupid = "0";
-    localStorage.setItem("stupid", stupid);
-}
+/* ^^^ LOCAL STORAGE ^^^ */
+/* ^^^ VARIABLE ^^^ */
 
-var footer = document.getElementById("footer");
-if (footer) {
-    var text0 = new Date().getFullYear();
-    var text1 = ". ";
-    var text2 = "";
-    var text3 = "Some Rights Reserved";
-    var text4 = ".";
-    l10nHelper(function(){}, function() {
-        text1 = " 版权所有。";
-        text2 = "/zh-cn/"
-        text3 = "保留部分权利";
-        text4 = "。";
-    });
-    footer.innerHTML = `© 2024-${text0} Arcsin Studios${text1}<a href="${text2}/copyright/" style="color:dodgerblue;">${text3}</a>${text4}`;
-}
+/* vvv INITIALIZATION vvv */
+initTheme();
+initPage();
+initStupid();
+/* ^^^ INITIALIZATION ^^^ */
 
-document.addEventListener("keydown", function(event) {
-    if (event.code === "KeyS") {
-        if (document.activeElement.tagName !== "INPUT") {
-            var searchBox = document.getElementById("search");
-            event.preventDefault();
-            searchBoxVisible = !searchBoxVisible;
-            if (searchBoxVisible) {
-                searchBox.style.display = "block";
-            }
-            else {
-                searchBox.style.display = "none";
-            }
-        }
-    }
-    else if (event.code === "KeyA" || event.code == "ArrowLeft") {
-        if (document.activeElement.tagName !== "INPUT") {
-            event.preventDefault();
-            prevPage();
-        }
-    }
-    else if (event.code === "KeyD" || event.code == "ArrowRight") {
-        if (document.activeElement.tagName !== "INPUT") {
-            event.preventDefault();
-            nextPage();
-        }
-    }
-});
+/* vvv LISTENER vvv */
+document.addEventListener("keydown", handleKeydown);
+/* ^^^ LISTENER ^^^ */
 
-function initTheme() {
-    var page = document.getElementById("page");
-    var footer = document.getElementById("footer");
-    var pageLeft = document.getElementById("page-left");
-    var pageRight = document.getElementById("page-right");
-    var theme = localStorage.getItem("theme");
-    if (theme === "dark") {
-        page.classList.remove("light-background");
-        page.classList.add("dark-background");
-        footer.classList.remove("black-text");
-        footer.classList.add("white-text");
-        if (pageLeft !== null && pageRight !== null) {
-            pageLeft.classList.remove("black-text");
-            pageRight.classList.remove("black-text");
-            pageLeft.classList.add("white-text");
-            pageRight.classList.add("white-text");
-        }
-    }
-    page.style.display = "block";
-}
-window.onload = initTheme;
-
+/* vvv FUNCTION vvv */
+/* vvv HELPER vvv */
 function l10nHelper(func0, func1) {
     if (window.location.pathname.startsWith("/zh-cn/")) {
         func1();
@@ -104,28 +72,153 @@ function l10nHelper(func0, func1) {
         func0();
     }
 }
+/* ^^^ HELPER ^^^ */
 
-function toggleTheme() {
-    var page = document.getElementById("page");
+/* vvv GENERATION vvv */
+function genLangBtn() {
+    var langBtn = document.getElementById("lang-btn");
+    if (langBtn) {
+        langBtn.setAttribute("type", "button");
+        langBtn.classList.add("lang-button");
+        langBtn.style.position = "absolute";
+        var text0;
+        var text1;
+        var text2;
+        l10nHelper(
+            function() {
+                text0 = "/zh-cn" + window.location.pathname;
+                text1 = 225;
+                text2 = 10;
+                langBtn.textContent = "中文";
+            },
+            function() {
+                text0 = window.location.pathname.substring(6);
+                text1 = 175;
+                text2 = 20;
+                langBtn.textContent = "English";
+            }
+        );
+        langBtn.setAttribute("onclick", `navigateTo("${text0}")`);
+        langBtn.style.right = `${text1}px`;
+        langBtn.style.top = `${text2}px`;
+    }
+}
+
+function genThemeBtn() {
+    var themeBtn = document.getElementById("theme-btn");
+    if (themeBtn) {
+        themeBtn.setAttribute("onclick", "toggleTheme()");
+        themeBtn.setAttribute("type", "button");
+        themeBtn.classList.add("theme-button");
+        themeBtn.style.position = "absolute";
+        themeBtn.style.right = "10px";
+        themeBtn.style.top = "10px";
+        l10nHelper(
+            function() {
+                themeBtn.textContent = "Toggle Theme";
+            },
+            function() {
+                themeBtn.textContent = "切换主题";
+            }
+        )
+    }
+}
+
+function genTitle() {
+    var title = document.getElementById("title");
+    var text0;
+    if (title) {
+        text0 = title.dataset.text;
+        if (!text0) {
+            text0 = "~ Arcsin Studios ~";
+        }
+        title.classList.add("gradient-text0");
+        title.style.textAlign = "center";
+        title.innerHTML = `<h1>${text0}</h1>`;
+    }
+}
+
+function genSubtitle() {
+    var subtitle = document.getElementById("subtitle");
+    var text0;
+    if (subtitle) {
+        text0 = subtitle.dataset.text;
+        if (!text0) {
+            l10nHelper(
+                function() {
+                    text0 = "Craft · Narrate · Code";
+                },
+                function() {
+                    text0 = "创造 · 叙述 · 程序";
+                }
+            );
+        }
+        subtitle.classList.add("gradient-text1");
+        subtitle.style.textAlign = "center";
+        subtitle.innerHTML = `<h2>${text0}</h2>`;
+    }
+}
+
+function genPageBtn() {
+    var pageBtn = document.getElementById("page-btn");
+    if (pageBtn) {
+        pageBtn.classList.add("container-wrapper");
+        pageBtn.innerHTML = "\
+            <p id=\"page-left\" class=\"black-text page-button\" style=\"left:-5vw;\" onclick=\"prevPage()\">⯇</p>\n\
+            <p id=\"page-right\" class=\"black-text page-button\" style=\"right:-5vw;\" onclick=\"nextPage()\">⯈</p>\
+        ";
+    }
+}
+
+function genSearch() {
+    var search = document.getElementById("search");
+    if (search) {
+        search.setAttribute("onkeypress", "submitJump(event)");
+        search.setAttribute("type", "text");
+        search.classList.add("search-box");
+        l10nHelper(
+            function(){
+                search.setAttribute("placeholder", "Jump to page...");
+            },
+            function() {
+                search.setAttribute("placeholder", "跳转至页面……");
+            }
+        );
+    }
+}
+
+function genFooter() {
     var footer = document.getElementById("footer");
-    var pageLeft = document.getElementById("page-left");
-    var pageRight = document.getElementById("page-right");
-    var theme = localStorage.getItem("theme");
-    if (theme === null || theme === "light") {
-        localStorage.setItem("theme", "dark");
+    if (footer) {
+        footer.classList.add("black-text");
+        var text0 = new Date().getFullYear();
+        var text1;
+        var text2;
+        var text3;
+        var text4;
+        l10nHelper(
+            function() {
+                text1 = ". ";
+                text2 = "";
+                text3 = "Some Rights Reserved";
+                text4 = ".";
+            },
+            function() {
+                text1 = " 版权所有。";
+                text2 = "/zh-cn"
+                text3 = "保留部分权利";
+                text4 = "。";
+            }
+        );
+        footer.innerHTML = `© 2024-${text0} Arcsin Studios${text1}<a href="${text2}/copyright/" style="color:dodgerblue;">${text3}</a>${text4}`;
     }
-    else {
-        localStorage.setItem("theme", "light");
-    }
-    page.classList.toggle("light-background");
-    page.classList.toggle("dark-background");
-    footer.classList.toggle("black-text");
-    footer.classList.toggle("white-text");
-    if (pageLeft !== null && pageRight !== null) {
-        pageLeft.classList.toggle("black-text");
-        pageRight.classList.toggle("black-text");
-        pageLeft.classList.toggle("white-text");
-        pageRight.classList.toggle("white-text");
+}
+/* ^^^ GENRERATION ^^^ */
+
+/* vvv PAGE vvv */
+function initPage() {
+    for (let i = minPage; i <= maxPage; ++i) {
+        pages.push(document.getElementById("page" + i));
     }
 }
 
@@ -153,12 +246,93 @@ function nextPage() {
     }
     showPage();
 }
+/* ^^^ PAGE ^^^ */
+
+/* vvv THEME vvv */
+function initTheme() {
+    if (theme === "dark") {
+        page.classList.remove("light-background");
+        page.classList.add("dark-background");
+        footer.classList.remove("black-text");
+        footer.classList.add("white-text");
+        if (pageLeft !== null && pageRight !== null) {
+            pageLeft.classList.remove("black-text");
+            pageRight.classList.remove("black-text");
+            pageLeft.classList.add("white-text");
+            pageRight.classList.add("white-text");
+        }
+    }
+    page.style.display = "block";
+}
+
+function toggleTheme() {
+    var page = document.getElementById("page");
+    var footer = document.getElementById("footer");
+    var pageLeft = document.getElementById("page-left");
+    var pageRight = document.getElementById("page-right");
+    var theme = localStorage.getItem("theme");
+    if (theme === null || theme === "light") {
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        localStorage.setItem("theme", "light");
+    }
+    page.classList.toggle("light-background");
+    page.classList.toggle("dark-background");
+    footer.classList.toggle("black-text");
+    footer.classList.toggle("white-text");
+    if (pageLeft !== null && pageRight !== null) {
+        pageLeft.classList.toggle("black-text");
+        pageRight.classList.toggle("black-text");
+        pageLeft.classList.toggle("white-text");
+        pageRight.classList.toggle("white-text");
+    }
+}
+/* ^^^ THEME ^^^ */
+
+/* vvv LISTENER vvv */
+function handleKeydown(event) {
+    if (event.code === "KeyS") {
+        if (document.activeElement.tagName !== "INPUT") {
+            var searchBox = document.getElementById("search");
+            event.preventDefault();
+            searchBoxVisible = !searchBoxVisible;
+            if (searchBoxVisible) {
+                searchBox.style.display = "block";
+            }
+            else {
+                searchBox.style.display = "none";
+            }
+        }
+    }
+    else if (event.code === "KeyA" || event.code == "ArrowLeft") {
+        if (document.activeElement.tagName !== "INPUT") {
+            event.preventDefault();
+            prevPage();
+        }
+    }
+    else if (event.code === "KeyD" || event.code == "ArrowRight") {
+        if (document.activeElement.tagName !== "INPUT") {
+            event.preventDefault();
+            nextPage();
+        }
+    }
+}
+/* ^^^ LISTENER ^^^ */
+
+/* vvv NAVIGATION vvv */
+function initStupid() {
+    if (stupid === null) {
+        stupid = "0";
+        localStorage.setItem("stupid", stupid);
+    }
+}
 
 function navigateTo(url) {
     window.location.href = url;
 }
 
-function getInputValue() {
+function getSearch() {
     var input = document.getElementById("search");
     return input.value;
 }
@@ -185,7 +359,7 @@ function checkMalicious(input) {
 }
 
 function submitJump(event) {
-    var input = getInputValue();
+    var input = getSearch();
     if (event.code === "Enter") {
         if (checkMalicious(input)) {
             if (input === "http://" || input === "https://") {
@@ -216,9 +390,12 @@ function submitJump(event) {
         }
     }
 }
+/* ^^^ NAVIGATION ^^^ */
+/* ^^^ FUNCTION ^^^ */
 
+// Woah, an Easter Egg!
 function checkEasterEgg() {
-    if (getInputValue() === "BABA IS YOU") {
+    if (getSearch() === "BABA IS YOU") {
         navigateTo("arcsin-is-us/");
     }
 }

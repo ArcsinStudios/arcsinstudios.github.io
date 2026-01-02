8b4a5d022594c77ad4f1b7fd6b3a6f30ec0b2751
Arcsin Studios <arcsinstudios@outlook.com>
2026-01-02 22:42:00 +0800

    Updated renderer

--- renderer/scripts.js
+++ renderer/scripts.js
@@ -23,13 +23,37 @@ class Point2D {
 
 class Face {
     constructor(points) {
-        this.points = points;
+        this.points = [];
+        for (let point of points) {
+            this.points.push(new Point3D(point.x, point.y, point.z));
+        }
     }
 }
 
 class MyObject {
     constructor(faces) {
         this.faces = faces;
+        let maxX = -Infinity;
+        let minX = Infinity;
+        let maxY = -Infinity;
+        let minY = Infinity;
+        let maxZ = -Infinity;
+        let minZ = Infinity;
+        for (let face of this.faces) {
+            for (let point of face.points) {
+                maxX = Math.max(maxX, point.x);
+                minX = Math.min(minX, point.x);
+                maxY = Math.max(maxY, point.y);
+                minY = Math.min(minY, point.y);
+                maxZ = Math.max(maxZ, point.z);
+                minZ = Math.min(minZ, point.z);
+            }
+        }
+        this.center = new Point3D(
+            (maxX + minX) / 2,
+            (maxY + minY) / 2,
+            (maxZ + minZ) / 2
+        );
     }
 }
 
@@ -56,36 +80,13 @@ function drawObject(object) {
     }
 }
 
-function getObjectCenter(object) {
-    let maxX = -Infinity;
-    let minX = Infinity;
-    let maxY = -Infinity;
-    let minY = Infinity;
-    let maxZ = -Infinity;
-    let minZ = Infinity;
-    for (let face of object.faces) {
-        for (let point of face.points) {
-            maxX = Math.max(maxX, point.x);
-            minX = Math.min(minX, point.x);
-            maxY = Math.max(maxY, point.y);
-            minY = Math.min(minY, point.y);
-            maxZ = Math.max(maxZ, point.z);
-            minZ = Math.min(minZ, point.z);
-        }
-    }
-    return new Point3D(
-        (maxX + minX) / 2,
-        (maxY + minY) / 2,
-        (maxZ + minZ) / 2
-    );
-}
-
 function moveObjectX(object, dist) {
     for (let face of object.faces) {
         for (let point of face.points) {
             point.x += dist;
         }
     }
+    object.center.x += dist;
 }
 
 function moveObjectY(object, dist) {
@@ -94,6 +95,7 @@ function moveObjectY(object, dist) {
             point.y += dist;
         }
     }
+    object.center.y += dist;
 }
 
 function moveObjectZ(object, dist) {
@@ -102,19 +104,19 @@ function moveObjectZ(object, dist) {
             point.z += dist;
         }
     }
+    object.center.z += dist;
 }
 
 function rotateObjectXY(object, deg) {
     let rad = deg * Math.PI / 180;
     let cos = Math.cos(rad);
     let sin = Math.sin(rad);
-    let center = getObjectCenter(object);
     for (let face of object.faces) {
         for (let point of face.points) {
-            let XBack = point.x - center.x;
-            let YBack = point.y - center.y;
-            point.x = XBack * cos - YBack * sin + center.x;
-            point.y = XBack * sin + YBack * cos + center.y;
+            let XBack = point.x - object.center.x;
+            let YBack = point.y - object.center.y;
+            point.x = XBack * cos - YBack * sin + object.center.x;
+            point.y = XBack * sin + YBack * cos + object.center.y;
         }
     }
 }
@@ -123,13 +125,12 @@ function rotateObjectYZ(object, deg) {
     let rad = deg * Math.PI / 180;
     let cos = Math.cos(rad);
     let sin = Math.sin(rad);
-    let center = getObjectCenter(object);
     for (let face of object.faces) {
         for (let point of face.points) {
-            let YBack = point.y - center.y;
-            let ZBack = point.z - center.z;
-            point.y = YBack * cos - ZBack * sin + center.y;
-            point.z = YBack * sin + ZBack * cos + center.z;
+            let YBack = point.y - object.center.y;
+            let ZBack = point.z - object.center.z;
+            point.y = YBack * cos - ZBack * sin + object.center.y;
+            point.z = YBack * sin + ZBack * cos + object.center.z;
         }
     }
 }
@@ -138,25 +139,24 @@ function rotateObjectZX(object, deg) {
     let rad = deg * Math.PI / 180;
     let cos = Math.cos(rad);
     let sin = Math.sin(rad);
-    let center = getObjectCenter(object);
     for (let face of object.faces) {
         for (let point of face.points) {
-            let ZBack = point.z - center.z;
-            let XBack = point.x - center.x;
-            point.z = ZBack * cos - XBack * sin + center.z;
-            point.x = ZBack * sin + XBack * cos + center.x;
+            let ZBack = point.z - object.center.z;
+            let XBack = point.x - object.center.x;
+            point.z = ZBack * cos - XBack * sin + object.center.z;
+            point.x = ZBack * sin + XBack * cos + object.center.x;
         }
     }
 }
 
-let point0 = new Point3D(-0.5, -0.5, 0);
-let point1 = new Point3D(-0.5, -0.5, 1);
-let point2 = new Point3D(-0.5,  0.5, 0);
-let point3 = new Point3D(-0.5,  0.5, 1);
-let point4 = new Point3D( 0.5, -0.5, 0);
-let point5 = new Point3D( 0.5, -0.5, 1);
-let point6 = new Point3D( 0.5,  0.5, 0);
-let point7 = new Point3D( 0.5,  0.5, 1);
+let point0 = new Point3D(-0.5, -0.5, -0.5);
+let point1 = new Point3D(-0.5, -0.5,  0.5);
+let point2 = new Point3D(-0.5,  0.5, -0.5);
+let point3 = new Point3D(-0.5,  0.5,  0.5);
+let point4 = new Point3D( 0.5, -0.5, -0.5);
+let point5 = new Point3D( 0.5, -0.5,  0.5);
+let point6 = new Point3D( 0.5,  0.5, -0.5);
+let point7 = new Point3D( 0.5,  0.5,  0.5);
 let cube = new MyObject([
     new Face([point1, point3, point7, point5]),
     new Face([point0, point4, point6, point2]),
@@ -166,21 +166,21 @@ let cube = new MyObject([
     new Face([point0, point2, point3, point1])
 ]);
 let dist = 0;
+moveObjectZ(cube, 1);
 
 function main() {
     canvasCtx.clearRect(0, 0, width, height);
-    drawObject(cube);
-    let center = getObjectCenter(cube);
-    if (center.z <= 1) {
-        dist = 0.01;
+    if (cube.center.z <= 1) {
+        dist = 0.05;
     }
-    else if (center.z >= 10) {
-        dist = -0.01;
+    else if (cube.center.z >= 10) {
+        dist = -0.05;
     }
     moveObjectZ(cube, dist);
-    rotateObjectXY(cube, 0.1);
-    rotateObjectYZ(cube, 0.1);
-    rotateObjectZX(cube, 0.1);
+    rotateObjectXY(cube, 0.5);
+    rotateObjectYZ(cube, 0.5);
+    rotateObjectZX(cube, 0.5);
+    drawObject(cube);
     requestAnimationFrame(main);
 }
 
